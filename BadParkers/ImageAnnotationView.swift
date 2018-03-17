9296b79550761010342485633602374587ecf007
Donahue Avila <donahue.avila@gmail.com>
2018-03-16 23:20:57 -0500

    Made our starting Annotation View

--- BadParkers/ImageAnnotationView.swift
+++ BadParkers/ImageAnnotationView.swift
@@ -11,22 +11,42 @@ import MapKit
 class ImageAnnotationView: MKAnnotationView {
     
     var url : URL?
+    var carImage: UIImage?
     
     override init(annotation: MKAnnotation?, reuseIdentifier: String?) {
         super.init(annotation: annotation, reuseIdentifier: reuseIdentifier)
         if let annotation = annotation as? ImageAnnotation {
             url = annotation.url
+            
+        }
+        frame = CGRect(x: 0, y: 0, width: 70, height: 70)
+        isOpaque = false
+        setNeedsDisplay()
+    }
+
+    
+    //TODO: Image fills instead of clips, could be changed database side?
+    override func draw(_ rect: CGRect) {
+        
+        let roundedRect = UIBezierPath(roundedRect: rect, cornerRadius: 10)
+        #colorLiteral(red: 1, green: 1, blue: 1, alpha: 1).setFill()
+        roundedRect.fill()
+
+        
+        if let drawImage = carImage {
+            drawImage.draw(in: rect.insetBy(dx: 5, dy: 5))
+        } else {
+            fetchImage()
         }
         
-        frame = CGRect(x: 0, y: 0, width: 30, height: 30)
-//        backgroundColor = UIColor.blue
-        fetchImage()
         
     }
     
+    //TODO: Figure out what to do when selected
     override func setSelected(_ selected: Bool, animated: Bool) {
         super.setSelected(selected, animated: animated)
         if selected {
+            setNeedsDisplay()
             print("Selected")
             //load bigger image or prepare for segue
         } else {
@@ -40,7 +60,8 @@ class ImageAnnotationView: MKAnnotationView {
                 [weak self] in let urlContents = try? Data(contentsOf: url)
                 DispatchQueue.main.async {
                     if let imageData = urlContents, url == self?.url {
-                        self?.image = UIImage(data: imageData)
+                        self?.carImage = UIImage(data: imageData)
+                        self?.setNeedsDisplay()
                     }
                 }
             }
